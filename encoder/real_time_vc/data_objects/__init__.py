1a867ee8bd2e677b0afb5167e0b31057310d7746
Aribaldi <corsypirate@gmail.com>
2021-05-05 18:22:05 +0300

    encoder added

---
+++ encoder/real_time_vc/data_objects/__init__.py
@@ -0,0 +1,2 @@
+from encoder.real_time_vc.data_objects.speaker_verification_dataset import SpeakerVerificationDataset
+from encoder.real_time_vc.data_objects.speaker_verification_dataset import SpeakerVerificationDataLoader
